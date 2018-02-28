939954389e090c8d84ef3cb752294f383b17d1d8
fabricio <fabricio@lbscanarias.es>
2018-02-28 09:26:26 +0000

    sdfsdf

--- commit.sh
+++ commit.sh
@@ -2,7 +2,9 @@
 git add -A  
 read -p "Commit description: " desc  
 git commit -m $desc  
+echo "**** PULL STARTED ****"
 git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master
-echo "pull"
+echo "---- PULL FINISHED ----"
+echo "**** PUSH STARTED ****"
 git push https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master
-echo "push"
+echo "---- PUSH STARTED ----"
