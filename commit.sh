2a3c60fa40517b51ce8ab0e169c088868c398385
fabricio <fabricio@lbscanarias.es>
2018-02-28 11:27:39 +0000

    a

--- commit.sh
+++ commit.sh
@@ -3,9 +3,7 @@ git add -A
 IFS= read -r -p "Commit message: " input
 git commit -m "$input"  
 echo "**** PULL STARTED ****"
-git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | output 
-echo ">>>>>>>>>>>>>>>>>>>>" $output
-echo "<<<<<<<<<<<<<<<<<<<<"
+output = git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l 
 echo "---- PULL FINISHED ----"
 echo "**** PUSH STARTED ****"
 git push https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l
