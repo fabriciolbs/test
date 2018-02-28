863541666943419094101cbe50fd39d73824e51c
fabricio <fabricio@lbscanarias.es>
2018-02-28 11:32:54 +0000

    aasd

--- commit.sh
+++ commit.sh
@@ -3,9 +3,11 @@ git add -A
 IFS= read -r -p "Commit message: " input
 git commit -m "$input"  
 echo "**** PULL STARTED ****"
-STORAGE=$(git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l)
-echo "STORAGE " $STORAGE
+lines=$(git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l)
 echo "---- PULL FINISHED ----"
+if [ $lines == 1 ]
+then
 echo "**** PUSH STARTED ****"
 git push https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l
 echo "---- PUSH STARTED ----"
+fi
