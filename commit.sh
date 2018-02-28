be98437a0377a685f71ed585d44893ab615cffb7
fabricio <fabricio@lbscanarias.es>
2018-02-28 11:34:54 +0000

    asd

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@ git add -A
 IFS= read -r -p "Commit message: " input
 git commit -m "$input"  
 echo "**** PULL STARTED ****"
-lines=$(git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l)
+lines = $(git pull https://fabriciolbs:lazaroj1@github.com/fabriciolbs/test.git master | wc -l)
 echo "---- PULL FINISHED ----"
 if [ $lines > 0 ]
 then
